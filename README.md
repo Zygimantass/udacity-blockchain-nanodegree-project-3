d41fa52cbfa911ea99991adb42f4eea1d34c408f
Zygimantass <zygimantelis@gmail.com>
2018-09-07 08:54:24 +0300

    first commit, reorganize repo

---
+++ README.md
@@ -0,0 +1,11 @@
+# Project 3: Private blockchain w/ webservice
+#### How to run:
+1. Run ```npm install```
+2. Use the command ```node server.js```
+3. Your server is going to run on port ```3000```
+
+### Endpoints:
+| Type | Endpoint   | Function                                                                  |
+|------|------------|---------------------------------------------------------------------------|
+| GET  | /block/:id | Returns a block by ID                                                     |
+| POST | /block/    | Creates a new block with data that's inside the post data with key "data" |
---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const Blockchain = require('./simpleChain').Blockchain;
+const Block = require('./simpleChain').Block;
+const dbInterface = require('./dbInterface')
+
+const version = require('./package.json').version;
+
+let blockChain = new Blockchain();
+
+app.get('/', (req, res) => {
+  res.json({'success': true, version: version})
+});
+
+app.get('/block/:blockHeight', function(req, res) {
+  let blockHeight = -1;
+  if (req.params.blockHeight === undefined) {
+    res.status(400).json({'success': false, 'error': 'Please include block ID in URL'});
+    return;
+  }
+  try {
+    blockHeight = parseInt(req.params.blockHeight);
+    if (isNaN(blockHeight)) {
+      throw "Block ID is invalid";
+    }
+  } catch (err) {
+    console.log(err)
+    res.status(400).json({'success': false, 'error': 'Block ID is invalid'})
+    return;
+  }
+
+  dbInterface.getBlock(blockHeight)
+  .then((block) => {
+    res.json({"success": true, "block": block})
+    return;
+  })
+  .catch((err) => {
+    res.status(500).json({"success": false, "error": err.toString()});
+    return;
+  })
+})
+
+app.post('/block/', function (req, res) {
+  let blockData = req.body.data;
+  if (blockData === undefined) {
+    blockData = "";
+  }
+
+  blockChain.addBlock(new Block(blockData))
+  .then((newBlock) => {
+    console.log(newBlock)
+    res.json({"success": true, "block": newBlock});
+    return; 
+  }).catch((err) => {
+    res.status(500).json({"success": false, "error": "Error while adding your block to the chain"});
+    return;
+  })
+});
+
+app.listen(3000, () => console.log('Blockchain web service running on port 3000'))
